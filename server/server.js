e173ad5e29a73dd766eef7a63d7c5adaa415b318
aahmad-fabriquenumerique <aahmad@fabriquenumerique.fr>
2024-06-19 15:39:44 +0200

    deploy

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config({path: 'config.env'})
+const express = require('express')
+const routes = require('../route/route')
+const mongoConnect = require('../connection/connect') 
+
+const PORT = process.env.PORT
+
+const app = express()
+app.use(express.json())
+app.use('/', routes)
+
+mongoConnect()
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
