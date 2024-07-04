cfdacb746f227a3800f255899813c7ced673ddc0
aahmad-fabriquenumerique <aahmad@fabriquenumerique.fr>
2024-07-04 16:26:41 +0200

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config({path: 'config.env'})
 const express = require('express')
-const routes = require('./route/route')
+c
 const mongoConnect = require('./connection/connect') 
 
 const PORT = 3000
@@ -15,4 +15,3 @@ app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
 
-module.exports = app
